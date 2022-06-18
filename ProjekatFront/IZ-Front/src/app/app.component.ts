71c7f381a2c1c07729b15d587ee13a1bb5167e90
Sladjana Colakovic <74436050+SladjanaColakovic@users.noreply.github.com>
2022-06-18 17:19:16 +0200

    homepage done

--- ProjekatFront/IZ-Front/src/app/app.component.ts
+++ ProjekatFront/IZ-Front/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,17 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'IZ-Front';
+  isHomePage = true;
+
+  constructor(public router: Router) { }
+
+  homePage(){
+    this.router.navigate(['/']);
+    this.isHomePage = true;
+  }
+
+  componentSuggestion(){
+    this.router.navigate(['/componentSuggestion']);
+    this.isHomePage = false;
+  }
 }
--- ProjekatFront/IZ-Front/src/app/app.module.ts
+++ ProjekatFront/IZ-Front/src/app/app.module.ts
@@ -11,7 +11,7 @@ import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 @NgModule({
   declarations: [
     AppComponent,
-    ComponentSuggestionComponent
+    ComponentSuggestionComponent,
   ],
   imports: [
     BrowserModule,
